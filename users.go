561fcbbd0ab75de226447a46c792ddeade5197f8
Kevin Gibson <kgibson@spotnana.com>
2024-01-17 14:28:01 -0800

    refactoring

--- users.go
+++ users.go
@@ -2,6 +2,7 @@ package jcclient
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 )
 
@@ -9,7 +10,9 @@ import (
 func (c *Client) GetUser(userId string) (user SystemUser, err error) {
 	c.HostURL.Path = "/api/systemusers/" + userId
 	req, err := http.NewRequest(http.MethodGet, c.HostURL.String(), nil)
-	body, _ := c.doRequest(req)
+	req.Header = c.Headers
+	response, _ := c.HTTPClient.Do(req)
+	body, _ := io.ReadAll(response.Body) // response body is []byte
 	err = json.Unmarshal(body, &user)
 	return user, err
 }
