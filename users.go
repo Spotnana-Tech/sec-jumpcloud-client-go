68172ee2cd03cbce775adf38e5331d56636b8962
Kevin Gibson <kgibson@spotnana.com>
2024-01-17 14:21:41 -0800

    refactoring

--- users.go
+++ users.go
@@ -6,7 +6,7 @@ import (
 )
 
 // GetUser is a struct that represents a Jumpcloud user group
-func (c *Client) GetUser(userId string) (user User, err error) {
+func (c *Client) GetUser(userId string) (user SystemUser, err error) {
 	c.HostURL.Path = "/api/systemusers/" + userId
 	req, err := http.NewRequest(http.MethodGet, c.HostURL.String(), nil)
 	body, _ := c.doRequest(req)
